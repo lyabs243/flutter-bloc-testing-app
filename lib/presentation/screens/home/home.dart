7c8d593a0f746a68d17c51da180393f6626fda3f
lyabs243 <loicyabili@outlook.com>
2021-11-20 11:44:43 +0200

    Create home page with loading state

--- lib/presentation/screens/home/home.dart
+++ lib/presentation/screens/home/home.dart
@@ -17,10 +17,60 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(MyLocalizations.instanceLocalization['app_title'])
+        title: Center(
+          child: Text(MyLocalizations.instanceLocalization['app_title']),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+
+            },
+            icon: Icon(
+              Icons.settings,
+              color: Colors.white,
+            )
+          )
+        ],
       ),
       body: Container(
-
+        padding: EdgeInsets.all(12),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(),
+                Text(
+                  '10:06:34',
+                  textScaleFactor: 1.5,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold
+                  ),
+                )
+              ],
+            ),
+            Container(
+              child: Column(
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    child: CircularProgressIndicator(),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.15,),
+                  Container(
+                    alignment: Alignment.center,
+                    child: Transform.scale(
+                      child: CircularProgressIndicator(),
+                      scale: 2,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(height: 10,)
+          ],
+        ),
       ),
     );
   }
